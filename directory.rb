9dd6ecac9ae7a72faf188e1fa9df801c4ef17314
Maiara <maiarinhah.eu@gmail.com>
2020-09-23 20:20:40 +0100

    Interactive menu

--- directory.rb
+++ directory.rb
@@ -1,17 +1,14 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-
-students = []
-
-name = gets.chomp
-
-while !name.empty? do
-  students << {name: name, cohort: :november}
-  puts "Now we have #{students.count} students"
+  students = []
   name = gets.chomp
-end
-students
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
 end
 
 def print_header
@@ -29,7 +26,26 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+   puts "1.Input the students"
+   puts "2.Show the students"
+   puts "9. Exit"
+   selection = gets.chomp
+   case selection
+   when "1"
+     students = input_students
+   when "2"
+     print_header
+     print(students)
+     print_footer(students)
+   when "9"
+     exit
+   else
+     puts "I don't know what you meant, try again"
+   end
+  end
+end
+# students = input_students
+interactive_menu
